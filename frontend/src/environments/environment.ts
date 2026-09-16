f082a1f4d5b67741d82bc8e26c699ada09eca563
dgiron-2025071 <dgiron-2025071@kinal.edu.gt>
2026-09-15 23:37:33 -0600

    chore: configurar el id de cliente de Google en el entorno de desarrollo (#38)

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://localhost:3000",
+  googleClientId: "807712069315-m6pci1bsud78vkt8tmf4496ve7bg2d9n.apps.googleusercontent.com",
+};
